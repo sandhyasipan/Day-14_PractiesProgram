56d46892a2bbf062ede0b96477d9e7f7cdb08d2b
sandhyasipan <sandhyaswain70@gmail.com>
2022-08-31 20:04:31 +0530

    added to UC1_SimpleLink

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.bridgelabs;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure and Algorithm program for Adding the Data ");
+
+        LinkedList list1 = new LinkedList();
+        list1.push(56);
+        list1.push(30);
+        list1.push(70);
+    }
+}
