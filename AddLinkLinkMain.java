dc392950fdcca500d0b868c549e69d740c5efe0a
sandhyasipan <sandhyaswain70@gmail.com>
2022-08-31 20:43:08 +0530

    added to UC2_AddLinkList

--- AddLinkLinkMain.java
+++ AddLinkLinkMain.java
@@ -1,2 +1,11 @@
-package com.bridgelabs;public class AddLinkLinkMain {
+package com.bridgelabs;
+
+public class AddLinkLinkMain {
+    public static void addData(){
+        LinkedList list = new LinkedList();
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        list.print();
+    }
 }
