1815dad11e183c6781a92b891307ae2886a427c0
sandhyasipan <sandhyaswain70@gmail.com>
2022-09-01 07:18:05 +0530

    added to  UC3_AppendList

---
+++ AppendLinkList.java
@@ -0,0 +1,47 @@
+package com.bridgelabs;
+
+public class AppendLinkList {
+    Node head;
+    Node tail;
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while(temp!=null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+}
